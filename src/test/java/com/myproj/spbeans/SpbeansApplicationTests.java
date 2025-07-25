4dc64f371a128e537987723486d7c283aef5c913
anilnqa <aniln.qa@gmail.com>
2025-07-25 13:18:22 +0530

    springboot all beans

---
+++ src/test/java/com/myproj/spbeans/SpbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproj.spbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
